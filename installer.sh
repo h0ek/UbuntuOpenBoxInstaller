47917ca255142a8d2bb16f4c11b6a728446fa335
hoek <hoek@tuta.io>
2019-09-03 13:32:14 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -41,10 +41,6 @@ echo "Adding User to vboxsf"
 sudo usermod -aG vboxsf $user
 #Installing all packages
 echo "Installing all packages"
-sudo apt -y install nano mc htop wget curl sakura git net-tools bash-completion openbox obmenu rofi xinit libpam0g-dev libxcb1-dev xorg xauth compton firefox geany flameshot lxappearance tint2 conky i3lock-fancy nomacs zip unzip unrar p7zip neofetch
-#Installing obmenu-generator
-wget https://download.opensuse.org/repositories/home:/Head_on_a_Stick:/obmenu-generator/Debian_Unstable/all/obmenu-generator_0.85-3_all.deb
-sudo apt -y install ./obmenu-generator_0.85-3_all.deb
+sudo apt -y install nano mc htop wget curl sakura git net-tools bash-completion openbox obmenu rofi xinit libpam0g-dev libxcb1-dev xorg xauth compton firefox geany flameshot lxappearance tint2 conky i3lock-fancy nomacs zip unzip unrar p7zip neofetch obmenu-generator
 read -rsn1 -p "Press any key to reboot";echo
-sudo rm ./obmenu-generator_0.85-3_all.deb
 sudo shutdown -r now
