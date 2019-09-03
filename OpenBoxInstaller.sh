24d44d76e05184390d360ce34cc2551a64fa902d
hoek <hoek@tuta.io>
2019-09-03 11:59:19 +0200

    Update OpenBoxInstaller.sh

--- OpenBoxInstaller.sh
+++ OpenBoxInstaller.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cat << "EOF"
  _____ ______  _____  _   _ ______  _____ __   __
 |  _  || ___ \|  ___|| \ | || ___ \|  _  |\ \ / /
@@ -18,8 +18,6 @@ echo "This script will install and configure OpenBox window manager
 with some software and customized configuration files. It works 
 only with clean and minimal Ubuntu installation. After all it will be
 light and clean OpenBox machine. More info on GITHUB."
-printf 'Press [ENTER] to continue...'
-read _
 #Check root
 #[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 #Install the packages required to build the VirtualBox Guest Additions
